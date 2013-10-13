202b99a99912bb1456bfe2f793a1ecebdac37250
re757575 <re757575@gmail.com>
2013-10-13 21:47:08 +0800

    modif index contorl

--- control.js
+++ control.js
@@ -1,11 +1,31 @@
 var http = require('http');
 var express = require('express');
 var socket = require('socket.io');
+var  path = require('path');
+
+
 
 var app = express();
 
-var server = http.createServer(app).listen((1234), function(){
-  console.log("Express server on)");
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  var msg = 'Express server start on http://localhost:' + app.get('port');
+  console.log(msg);
 });
 
 var io = socket.listen(server);
